dade3adfb859ab70dff9c591b2adf856f3db0a1b
Georgy Ostroumov <gostroum@c2r6s5.42tokyo.jp>
2025-01-15 21:41:47 +0900

    added test all files

---
+++ test_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+norminette ../c04/*/ft*.c
+echo
+
+for i in ex{00..00}
+do
+	echo "$i Compiling..."
+	cc -Wall -Werror -Wextra $i/*.c ../c04/$i/*.c -o compile_$i
+	echo "$i Compiled! Executing..."
+	./compile_$i
+	echo "$i Finished!"
+	echo
+done
